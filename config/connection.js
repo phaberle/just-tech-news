832ed35f561d023e8b5277a7d74dab126e34faf7
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-13 23:01:07 -0500

    added local/heroku connection params

--- config/connection.js
+++ config/connection.js
@@ -4,26 +4,26 @@ require('dotenv').config();
 
 
 //Heroku
-let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequalize = new Sequalize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER.process.env.DB_Password, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
-}
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//     sequalize = new Sequalize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER.process.env.DB_Password, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
 
 
-/*
-local connection 
+
+ //local
 //create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequalize('just_tech_news_db', 'techUser', '{tzC]h}!]3_8)f"q',{
     host:'localhost',
     dialect: 'mysql',
     port: '3306'
 });
-*/
+
 
 module.exports = sequelize;
