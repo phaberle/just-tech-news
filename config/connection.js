aec5c4f66e59a742b07e17f006d7062d471fd18d
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-18 23:33:46 -0500

    changed connection config - new credentials

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequalize = require('sequelize');
 require('dotenv').config();
 
 //create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequalize('just_tech_news_db', 'techUser', '{tzC]h}!]3_8)f"q',{
+const sequelize = new Sequalize('just_tech_news', 'techuser', 'techuser',{
     host:'localhost',
     dialect: 'mysql',
     port: '3306',
