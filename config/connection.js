768ccf95888b039086cb61e6480939ad4ca9e1ff
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-25 11:41:15 -0500

    adding heroku config setttings

--- config/connection.js
+++ config/connection.js
@@ -5,26 +5,26 @@ require('dotenv').config();
 
 //Heroku
 // let sequelize;
-// if (process.env.JAWSDB_URL) {
-//     sequalize = new Sequalize(process.env.JAWSDB_URL);
-// } else {
-//     sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER.process.env.DB_Password, {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//     });
-// }
+if (process.env.JAWSDB_URL) {
+    sequalize = new Sequalize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER.process.env.DB_Password, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 
 
  //local
 //create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequalize('just_tech_news', 'techuser', 'techuser',{
-    host:'localhost',
-    dialect: 'mysql',
-    port: '3306',
-    logging:false //<-- TURN OFF VERBOSE SQL LOGGING!!!
-});
+// const sequelize = new Sequalize('just_tech_news', 'techuser', 'techuser',{
+//     host:'localhost',
+//     dialect: 'mysql',
+//     port: '3306',
+//     logging:false //<-- TURN OFF VERBOSE SQL LOGGING!!!
+// });
 
 
 module.exports = sequelize;
